9de98ea3f7a26d8c98bbaa53a0db8a3b5119e20e
Oleg Samarin <osamarin@openwaygroup.com>
2021-07-18 00:44:17 +0300

    - Switched to 1.9.19 - Removed obsolete build actions

--- build-scripts/for-win64/build-on-ubuntu.sh
+++ build-scripts/for-win64/build-on-ubuntu.sh
@@ -27,35 +27,16 @@ MINGW64_PREFIX=/usr/x86_64-w64-mingw32
 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ ./waf configure  --platform=win32 --winmme --portaudio --prefix=$MINGW64_PREFIX
 ./waf build
 
-
-
-(echo prefix=$MINGW64_PREFIX
- echo 'exec_prefix=${prefix}'
- echo 'libdir=${prefix}/lib'
- echo 'includedir=${prefix}/include'
- echo 'server_libs=-L${libdir} -ljackserver'
- echo
- echo Name: jack
- echo Description: the Jack Audio Connection Kit: a low-latency synchronous callback-based media server
- echo Version: $VERSION-$BUILD_VERSION
- echo 'Libs: -L${libdir} -ljack'
- echo 'Cflags: -I${includedir}'
-) > build/jack.pc
-
 # install
 PKG_DIR=`pwd`/${DEBIAN_PKG_NAME}_${VERSION}-${BUILD_VERSION}_all
 
-DESTDIR=${PKG_DIR}${MINGW64_PREFIX} ./waf install
-mkdir -p ${PKG_DIR}${MINGW64_PREFIX}/lib/jack
-mv ${PKG_DIR}${MINGW64_PREFIX}/lib/*.a ${PKG_DIR}${MINGW64_PREFIX}/lib/jack/
-mv ${PKG_DIR}${MINGW64_PREFIX}/lib/jack/libj* ${PKG_DIR}${MINGW64_PREFIX}/lib/
+DESTDIR=${PKG_DIR} ./waf install
 
 mkdir $PKG_DIR/DEBIAN
 
 cat >$PKG_DIR/DEBIAN/control <<EOF
 Package: $DEBIAN_PKG_NAME
-Version: $VERSION
-Revision: $BUILD_VERSION
+Version: $VERSION-$BUILD_VERSION
 Architecture: all
 Maintainer: Oleg Samarin <osamarin68@gmail.com>
 Description: This is jack audio connection kit for cross-compiling for for Win64
